d0e16ffcd8d255dd061e672f7e0feed6ff9233b0
gregoryw3 <greg.wainer@gmail.com>
2021-08-23 20:38:29 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/* Hello World
+Gregory Wainer
+August 23, 2021
+CMSC 255, Seciont 004*/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Gregory Wainer");
+        System.out.println("Hello.java");
+        System.out.println("Does not print hello world :( ");
+        System.out.println("August 23, 2021");
+        System.out.println("CMSC 255, 004");
+        System.out.println();
+    }
+}
